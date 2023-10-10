adda4b1cc5426aaee7dcbdc12550fcd3545ae3db
WarisAmir <warisamir1918@gmail.com>
2023-10-10 21:19:04 +0530

    Create binary number from linkedlist to integer.java

---
+++ binary number from linkedlist to integer.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        ListNode temp=head;
+        StringBuilder sb=new StringBuilder();
+     while(temp!=null){
+         sb.append(temp.val);
+         temp=temp.next;
+     } 
+     return Integer.parseInt(sb.toString(),2);
+    }
+}
